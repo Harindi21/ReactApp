a4c05725aa9946bbd649efbbd40c897be0df1725
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2024-01-05 01:02:41 +0530

    Create TopicBox.jsx

---
+++ src/components/TopicBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function TopicBox()
+{
+    return (
+        <div>
+            <div className="topicBox">
+                <span className="txt">This is the topic</span>
+            </div>
+        </div>
+    )
+}
